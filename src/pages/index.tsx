0ba6e3b4252d4c7658ed9f38df3823aa6516fa6c
Nathan Osti <nathanosti.job@gmail.com>
2022-04-01 11:34:26 -0300

    ✨ Add Head in index page to pass title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>IG News - Home</title>
+      </Head>
       <div className={styles.title}>Hello World</div>
     </>
   );
