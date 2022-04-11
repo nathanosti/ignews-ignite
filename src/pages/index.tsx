4bb32535944eb2d3d7d6961c0a6bf3b6a7848bb6
Nathan Osti <nathanosti.job@gmail.com>
2022-04-11 14:49:33 -0300

    ✨ Create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import styles from "../styles/home.module.scss";
+import Image from "next/image";
+import Styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
@@ -7,7 +8,31 @@ export default function Home() {
       <Head>
         <title>IG News - Home</title>
       </Head>
-      <div className={styles.title}>Hello World</div>
+
+      <main className={Styles.mainWrapper}>
+        <section>
+          <span>👏 Hey, welcome</span>
+
+          <h1>
+            News about the <b>React</b> world
+          </h1>
+
+          <p>
+            Get acces to all publictations <br /> <b>for $9,90 month</b>
+          </p>
+
+          <button>Subscribe now</button>
+        </section>
+
+        <div className={Styles.imageWrapper}>
+          <Image
+            layout="fill"
+            src="/assets/avatar.svg"
+            alt="IG News Ilustration"
+            objectFit="contain"
+          />
+        </div>
+      </main>
     </>
   );
 }
