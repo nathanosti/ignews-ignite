2348264639128438319bb7ac14f1e3f76cf791b6
Nathan Osti <nathanosti.job@gmail.com>
2022-04-11 14:49:07 -0300

    ♻️ Pass Next/Image for header logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,31 @@
+import { SignInButton } from "../SignInButton";
+import Image from "next/image";
+
+import Styles from "./styles.module.scss";
+
 export function Header() {
   return (
     <>
-      <header>
-        <img src="assets/logo.svg" alt="IG News - Logo" />
+      <header className={Styles.headerContainer}>
+        <div className={Styles.headerContent}>
+          <Image
+            width={"100%"}
+            height={'100%'}
+            src="/assets/logo.svg"
+            alt="IG News - Logo"
+          />
+
+          <nav>
+            <a className={Styles.active} href="/index" target="_blank">
+              Home
+            </a>
+            <a href="/posts" target="_blank">
+              Posts
+            </a>
+          </nav>
+
+          <SignInButton />
+        </div>
       </header>
     </>
   );
