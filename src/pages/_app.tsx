40851e9571c030d153c484a0df0df8c7b2193971
Nathan Osti <nathanosti.job@gmail.com>
2022-04-21 23:17:01 -0300

    :sparkles: Add and Configure Next Auth in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
+import { SessionProvider } from "next-auth/react";
 
 import "../styles/global.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Header />
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
 }
