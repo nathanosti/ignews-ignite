b8ac0bb95cbf05638b8004e746ad376a259907a5
Nathan Osti <nathanosti.job@gmail.com>
2022-04-21 23:22:40 -0300

    :recycle: remove status conditional

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -3,29 +3,21 @@ import Styles from "./styles.module.scss";
 import { signIn, useSession } from "next-auth/react";
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
-
-  const { data: session, status } = useSession();
-
-  console.log({ session }, `Status: ${status}`);
-
-  const username = "Nathan Osti";
+  const { data: session } = useSession();
 
   return (
     <>
       <button
         className={Styles.signButton}
         type="button"
-        onClick={status !== "authenticated" && (() => signIn("github"))}
+        onClick={!session && (() => signIn("github"))}
       >
         <FaGithub
           style={{ marginRight: "16px" }}
-          color={status === "authenticated" ? "#04D361" : "#EBA417"}
+          color={session ? "#04D361" : "#EBA417"}
           size={24}
         />
-        {status === "authenticated"
-          ? `${session.user.name}`
-          : "Sigin using GitHub"}
+        {session ? `${session.user.name}` : "Sigin using GitHub"}
       </button>
     </>
   );
